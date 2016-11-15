36d23191ed37ea72340d02bda370ee168ec95168
egandara <egandara@me.com>
2016-11-15 10:44:25 -0300

    table

--- Mapeos.Web/Estructura.aspx
+++ Mapeos.Web/Estructura.aspx
@@ -1,5 +1,23 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterMapeos.Master" AutoEventWireup="true" CodeBehind="Estructura.aspx.cs" Inherits="Mapeos.Web.Estructura" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-</asp:Content>
+    </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    
+    <asp:Panel ID="pblEstructuras" runat="server">
+        <asp:Table ID="miTabla" runat="server" BorderStyle="None">
+            <asp:TableRow>
+                <asp:TableHeaderCell>Nº</asp:TableHeaderCell>
+                <asp:TableHeaderCell>Nombre</asp:TableHeaderCell>
+                <asp:TableHeaderCell>Null</asp:TableHeaderCell>
+                <asp:TableHeaderCell>Descripción</asp:TableHeaderCell>
+                <asp:TableHeaderCell>_</asp:TableHeaderCell>
+            </asp:TableRow>
+        </asp:Table>
+        <asp:Button ID="btnAdd" runat="server" Text="+" OnClick="btnAdd_Click" />
+        <br />
+        <br />
+        <br />
+        <br />
+    </asp:Panel>
+    
 </asp:Content>
--- Mapeos.Web/Estructura.aspx.cs
+++ Mapeos.Web/Estructura.aspx.cs
@@ -9,9 +9,81 @@ namespace Mapeos.Web
 {
     public partial class Estructura : System.Web.UI.Page
     {
+        private int numOfRows = 1;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                GenerateTable(numOfRows);
+            }
+        }
 
+        private void GenerateTable(int rowsCount)
+        {
+            //Creat the Table and Add it to the Page
+            Table table = new Table();
+            table.ID = "Table1";
+            Page.Form.Controls.Add(table);
+
+            //The number of Columns to be generated
+            const int colsCount = 5;//You can changed the value of 3 based on you requirements
+
+            // Now iterate through the table and add your controls
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                TableRow row = new TableRow();
+                for (int j = 0; j < colsCount; j++)
+                {
+                    TableCell cell = new TableCell();
+                    TextBox tb = new TextBox();
+
+                    // Set a unique ID for each TextBox added
+                    tb.ID = "TextBoxRow_" + i + "Col_" + j;
+                    // Add the control to the TableCell
+                    cell.Controls.Add(tb);
+                    // Add the TableCell to the TableRow
+                    row.Cells.Add(cell);
+                }
+
+                // And finally, add the TableRow to the Table
+                miTabla.Rows.Add(row);
+            }
+
+            //Set Previous Data on PostBacks
+            SetPreviousData(rowsCount, colsCount);
+
+            //Sore the current Rows Count in ViewState
+            rowsCount++;
+            ViewState["RowsCount"] = rowsCount;
+        }
+        
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (ViewState["RowsCount"] != null)
+            {
+                numOfRows = Convert.ToInt32(ViewState["RowsCount"].ToString());
+                GenerateTable(numOfRows);
+            }
+        }
+
+        private void SetPreviousData(int rowsCount, int colsCount)
+        {
+            Table table = (Table)Page.FindControl("Table1");
+            if (table != null)
+            {
+                for (int i = 0; i < rowsCount; i++)
+                {
+                    for (int j = 0; j < colsCount; j++)
+                    {
+                        //Extracting the Dynamic Controls from the Table
+                        TextBox tb = (TextBox)table.Rows[i].Cells[j].FindControl("TextBoxRow_" + i + "Col_" + j);
+                        //Use Request objects for getting the previous data of the dynamic textbox
+                        tb.Text = Request.Form["TextBoxRow_" + i + "Col_" + j];
+                    }
+                }
+            }
         }
     }
 }
